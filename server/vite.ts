89ebb8656c9a07f9a426c92f4ee96d542b14f833
AstolofoBunny <166725995+AstolofoBunny@users.noreply.github.com>
2025-08-02 22:12:51 +0300

    Update vite.ts

--- server/vite.ts
+++ server/vite.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { setupVite, serveStatic } from "./vite";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
